642232988c93e560884f7f6ba2c2b33962456487
Himanshu72060 <himanshusharma00898@gmail.com>
2023-04-11 09:38:28 +0530

    yes

--- routers/reqsend.js
+++ routers/reqsend.js
@@ -4,32 +4,26 @@ const schema = require("../models/schema/signup_schema");
 
 const reqsend = async (req, res) => {
     let { srchuser } = req.params;
+
     const searchuser = await schema.find({
         $or: [{ firstName: { $regex: srchuser } }, { email: { $regex: srchuser } }],
     });
-    console.log(searchuser[0]._id, "ll");
-    if (searchuser.length !== 0) {
-        const findId = searchuser[0]._id;
+    const findId = searchuser[0]._id;
+    const Usercheck = await reqschema.find({ sendTo: findId });
+    if (Usercheck.length === 0) {
+
         const sendreq = new reqschema({
             sendBy: req.body.sendBy,
             sendTo: findId,
-        })
-        if (searchuser.length === 0) {
-            searchuser[0]
-        }
-        else {
-            return res.json({
-                msg: "cansal req"
-            })
-        }
+        });
         const saveData = await sendreq.save();
         return res.json({
             msg: "send req",
             data: saveData,
         })
     }
-
+    else {
+        res.json({ msg: "already sent frnd req to this user" });
+    };
 };
 module.exports = reqsend;
-
-
